63e003dc196b1e1488122004d64a9f362a4a7be2
Moazam05 <salmanmoazam08@gmail.com>
2024-01-25 23:47:13 +0500

    refactor code

---
+++ src/views/Brands/components/DeleteUserFromBrand.tsx
@@ -0,0 +1,116 @@
+import { Dialog } from "primereact/dialog";
+import { Image } from "primereact/image";
+import React from "react";
+import closeIcon from "../../../assets/images/close-icon.svg";
+import { Button } from "primereact/button";
+import DotLoader from "../../../components/Spinner/dotLoader";
+import { useRemoveUserFromTenantMutation } from "../../../redux/api/brandApiSlice";
+import ToastAlert from "../../../components/ToastAlert";
+
+interface DeleteUserFromBrandProps {
+  visible: boolean;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedUser: any;
+  title: string;
+  id: any;
+}
+
+const DeleteUserFromBrand: React.FC<DeleteUserFromBrandProps> = (props) => {
+  const { visible, setVisible, selectedUser, title, id } = props;
+
+  // Close Popup Icon
+  const closeIconTemplate = <Image src={closeIcon} alt="close icon" />;
+
+  // REMOVE USER API BIND
+  const [removeUserApi, { isLoading: removeUserLoading }] =
+    useRemoveUserFromTenantMutation();
+
+  const removeUser = async (user: any) => {
+    const payload = {
+      userId: user.id,
+      tenantId: id,
+    };
+
+    try {
+      const user: any = await removeUserApi(payload);
+
+      if (user?.data === null) {
+        ToastAlert("User Deleted Successfully", "success");
+        setVisible(false);
+      }
+
+      if (user?.error) {
+        ToastAlert(user?.error?.data?.title, "error");
+      }
+    } catch (error) {
+      console.error("Deleting User Error:", error);
+      ToastAlert("Something went wrong", "error");
+    }
+  };
+
+  return (
+    <>
+      <Dialog
+        visible={visible}
+        onHide={() => setVisible(false)}
+        style={{ width: "50vw" }}
+        breakpoints={{ "960px": "75vw", "641px": "100vw" }}
+        headerStyle={{ padding: 0 }}
+        closeIcon={closeIconTemplate}
+        contentClassName="p-0"
+      >
+        <div className="px-[104px] py-16">
+          <div className="text-center">
+            <h2 className="mb-6 text-4xl text-gray-200 font-semibold">
+              Are you sure?
+            </h2>
+            <p className="text-[22px] text-gray-200">
+              You are about to delete the user:{" "}
+              <span className="text-blue font-semibold">
+                {selectedUser?.userName}
+              </span>{" "}
+              <br />
+              from the brand{" "}
+              <span className="text-blue font-semibold">{title}</span>
+            </p>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-end gap-6 mt-10">
+              <Button
+                type="button"
+                className="theme-btn-default"
+                label="Cancel"
+                onClick={() => setVisible(false)}
+                disabled={removeUserLoading}
+              />
+
+              {removeUserLoading ? (
+                <div
+                  className="theme-btn leading-none"
+                  style={{
+                    height: "55px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <DotLoader color="#fff" size={12} />
+                </div>
+              ) : (
+                <Button
+                  type="button"
+                  className="theme-btn"
+                  label="Yes, delete this user"
+                  onClick={() => removeUser(selectedUser)}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </Dialog>
+    </>
+  );
+};
+
+export default DeleteUserFromBrand;
--- src/views/Brands/components/EditBrand.tsx
+++ src/views/Brands/components/EditBrand.tsx
@@ -6,21 +6,13 @@ import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Button } from "primereact/button";
 import { Dropdown } from "primereact/dropdown";
-import { Dialog } from "primereact/dialog";
-
-import closeIcon from "../../../assets/images/close-icon.svg";
 import BrandLogoImg from "../../../assets/images/banana.png";
-import { Image } from "primereact/image";
 import Breadcrumb from "../../../components/Breadcrumb/Index";
 import Sidebar from "../../../components/Sidebar";
-import {
-  useGetTenantUsersQuery,
-  useRemoveUserFromTenantMutation,
-} from "../../../redux/api/brandApiSlice";
+import { useGetTenantUsersQuery } from "../../../redux/api/brandApiSlice";
 import OverlayLoader from "../../../components/Spinner/OverlayLoader";
 import { capitalizeFirstLetter, formatDate } from "../../../utils";
-import ToastAlert from "../../../components/ToastAlert";
-import DotLoader from "../../../components/Spinner/dotLoader";
+import DeleteUserFromBrand from "./DeleteUserFromBrand";
 
 interface UserDT {
   userName: any;
@@ -68,42 +60,12 @@ const EditBrand = () => {
     { name: "Option C", code: "3" },
   ];
 
-  // REMOVE USER API BIND
-  const [removeUserApi, { isLoading: removeUserLoading }] =
-    useRemoveUserFromTenantMutation();
-
-  const removeUser = async (user: any) => {
-    const payload = {
-      userId: user.id,
-      tenantId: id,
-    };
-
-    try {
-      const user: any = await removeUserApi(payload);
-
-      if (user?.data === null) {
-        ToastAlert("User Deleted Successfully", "success");
-        setVisible(false);
-      }
-
-      if (user?.error) {
-        ToastAlert(user?.error?.data?.title, "error");
-      }
-    } catch (error) {
-      console.error("Deleting User Error:", error);
-      ToastAlert("Something went wrong", "error");
-    }
-  };
-
   // Table body templates
   const actionBodyTemplate = (data: UserDT) => {
     // console.log(data);
 
     return <Button label="Delete" text onClick={() => deleteUser(data)} />;
   };
-
-  // Close Popup Icon
-  const closeIconTemplate = <Image src={closeIcon} alt="close icon" />;
 
   // Handlers
   const deleteUser = (data: UserDT) => {
@@ -168,66 +130,14 @@ const EditBrand = () => {
             </div>
           </div>
 
-          {/* Confirm Popup */}
-          <Dialog
+          {/* Delete User Modal */}
+          <DeleteUserFromBrand
             visible={visible}
-            onHide={() => setVisible(false)}
-            style={{ width: "50vw" }}
-            breakpoints={{ "960px": "75vw", "641px": "100vw" }}
-            headerStyle={{ padding: 0 }}
-            closeIcon={closeIconTemplate}
-            contentClassName="p-0"
-          >
-            <div className="px-[104px] py-16">
-              <div className="text-center">
-                <h2 className="mb-6 text-4xl text-gray-200 font-semibold">
-                  Are you sure?
-                </h2>
-                <p className="text-[22px] text-gray-200">
-                  You are about to delete the user:{" "}
-                  <span className="text-blue font-semibold">
-                    {selectedUser?.userName}
-                  </span>{" "}
-                  <br />
-                  from the brand{" "}
-                  <span className="text-blue font-semibold">{title}</span>
-                </p>
-              </div>
-
-              <div>
-                <div className="flex items-center justify-end gap-6 mt-10">
-                  <Button
-                    type="button"
-                    className="theme-btn-default"
-                    label="Cancel"
-                    onClick={() => setVisible(false)}
-                    disabled={removeUserLoading}
-                  />
-
-                  {removeUserLoading ? (
-                    <div
-                      className="theme-btn leading-none"
-                      style={{
-                        height: "55px",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                      }}
-                    >
-                      <DotLoader color="#fff" size={12} />
-                    </div>
-                  ) : (
-                    <Button
-                      type="button"
-                      className="theme-btn"
-                      label="Yes, delete this user"
-                      onClick={() => removeUser(selectedUser)}
-                    />
-                  )}
-                </div>
-              </div>
-            </div>
-          </Dialog>
+            setVisible={setVisible}
+            selectedUser={selectedUser}
+            title={title}
+            id={id}
+          />
         </div>
       </div>
     </>
