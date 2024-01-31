cac3bb7a8818aa7e5e3ad5c68efe87c0dbe45fde
Moazam05 <salmanmoazam08@gmail.com>
2024-01-31 14:59:55 +0500

    creating mobile users

--- src/views/Users/mobile/MobileUsers.tsx
+++ src/views/Users/mobile/MobileUsers.tsx
@@ -1,12 +1,161 @@
+// React Imports
+import { useState } from "react";
+// Prime React Imports
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+// Redux
+import { useCreateUserMutation } from "../../../redux/api/userApiSlice";
+// Custom
 import MobileSideBar from "../../../components/MobileSideBar";
 import Header from "../../../components/Header";
+import Title from "../../../components/Title";
+import DotLoader from "../../../components/Spinner/dotLoader";
+import ToastAlert from "../../../components/ToastAlert";
+// Assets
+import RoundImage from "../../../assets/images/users_logo.svg";
 
 const MobileUsers = () => {
+  const [openCard, setOpenCard] = useState(false);
+  const [formData, setFormData] = useState({
+    userName: "",
+    email: "",
+  });
+
+  // CREATE USER API BIND
+  const [createNewUser, { isLoading }] = useCreateUserMutation();
+
+  const CreateUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const payload = {
+      userName: formData.userName,
+      email: formData.email,
+    };
+
+    try {
+      const userResponse: any = await createNewUser(payload);
+
+      if (userResponse?.data) {
+        setOpenCard(false);
+        setFormData({
+          userName: "",
+          email: "",
+        });
+        ToastAlert("User Created Successfully", "success");
+      }
+      if (userResponse?.error) {
+        ToastAlert(userResponse?.error?.data?.title, "error");
+      }
+    } catch (error) {
+      console.error("Creating User Error:", error);
+      ToastAlert("Something went wrong", "error");
+    }
+  };
+
   return (
     <div>
       <MobileSideBar />
       <Header />
-      <div className="my-2">Users</div>
+
+      <div className="my-2">
+        <Title brand={false} title="Users" image={RoundImage} />
+        <div className={`${openCard ? "mt-6" : "my-0"}`}>
+          <div
+            className={` ${openCard ? "p-4" : "pt-4 pb-4"} ${
+              openCard ? "bg-white" : "bg-transparent"
+            } rounded-lg  ${openCard ? "mb-6" : ""}`}
+          >
+            {openCard && (
+              <>
+                <div className="flex justify-between items-center">
+                  <div className="flex-1">
+                    <form className="" onSubmit={CreateUser}>
+                      <div className="mb-4">
+                        <InputText
+                          id="brandId"
+                          placeholder="User Name"
+                          className="theme-input"
+                          style={{
+                            width: "100%",
+                          }}
+                          value={formData.userName}
+                          onChange={(e) => {
+                            setFormData({
+                              ...formData,
+                              userName: e.target.value,
+                            });
+                          }}
+                        />
+                      </div>
+                      <div className="mb-2">
+                        <InputText
+                          id="website"
+                          placeholder="Email Address"
+                          className="theme-input"
+                          style={{
+                            width: "100%",
+                          }}
+                          value={formData.email}
+                          onChange={(e) => {
+                            setFormData({
+                              ...formData,
+                              email: e.target.value,
+                            });
+                          }}
+                        />
+                      </div>
+                      <Button
+                        type="button"
+                        label="Cancel"
+                        className="theme-btn-default leading-none w-full my-4"
+                        onClick={() => {
+                          setOpenCard(false);
+                          setFormData({
+                            userName: "",
+                            email: "",
+                          });
+                        }}
+                      />
+                      {isLoading ? (
+                        <div
+                          className="theme-btn"
+                          style={{
+                            height: "55px",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                          }}
+                        >
+                          <DotLoader color="#fff" size={12} />
+                        </div>
+                      ) : (
+                        <Button
+                          type="submit"
+                          disabled={isLoading}
+                          className="theme-btn w-full"
+                          label="Create"
+                        />
+                      )}
+                    </form>
+                  </div>
+                </div>
+              </>
+            )}
+            {!openCard && (
+              <Button
+                className="theme-btn w-full text-center flex justify-center"
+                onClick={() => {
+                  setOpenCard(true);
+                }}
+              >
+                + New User
+              </Button>
+            )}
+          </div>
+          {/* <div>
+            <MobileBrandList />
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 };
