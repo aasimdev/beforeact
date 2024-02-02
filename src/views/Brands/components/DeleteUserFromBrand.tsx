3b115e898eeca5ee17ba81715b6ff33b6d9e0962
Moazam05 <salmanmoazam08@gmail.com>
2024-02-02 22:03:17 +0500

    brand delete modal

--- src/views/Brands/components/DeleteUserFromBrand.tsx
+++ src/views/Brands/components/DeleteUserFromBrand.tsx
@@ -56,78 +56,126 @@ const DeleteUserFromBrand: React.FC<DeleteUserFromBrandProps> = (props) => {
         onHide={() => setVisible(false)}
         style={{ width: "50vw" }}
         breakpoints={{ "960px": "75vw", "641px": "100vw" }}
-        headerStyle={{ padding: 0 }}
+        headerStyle={{ padding: 0, display: mobile ? "none" : "block" }}
         closeIcon={closeIconTemplate}
-        contentClassName="p-0"
+        contentClassName={mobile ? "m-4 p-0 rounded-lg" : "p-0"}
       >
-        <div
-          className={`${mobile ? "px-[30px]" : "px-[104px]"} ${
-            mobile ? "py-4" : "py-16"
-          }`}
-        >
-          <div className="text-center">
-            <h2
-              className={`mb-6 ${
-                mobile ? "text-3xl" : "text-4xl"
-              } text-gray-200 font-semibold`}
-            >
-              Are you sure?
-            </h2>
-            <p
-              className={`${
-                mobile ? "text-[14px]" : "text-[22px]"
-              } text-gray-200`}
-            >
-              You are about to delete the user:{" "}
-              <span className="text-blue font-semibold">
-                {selectedUser?.userName}
-              </span>{" "}
-              <br />
-              from the brand{" "}
-              <span className="text-blue font-semibold">{title}</span>
-            </p>
-          </div>
+        {!mobile ? (
+          <>
+            <div className="px-[104px] py-16">
+              <div className="text-center">
+                <h2 className="mb-6 text-4xl text-gray-200 font-semibold">
+                  Are you sure?
+                </h2>
+                <p className="text-[22px] text-gray-200">
+                  You are about to delete the user:{" "}
+                  <span className="text-blue font-semibold">
+                    {selectedUser?.userName}
+                  </span>{" "}
+                  <br />
+                  from the brand{" "}
+                  <span className="text-blue font-semibold">{title}</span>
+                </p>
+              </div>
 
-          <div>
-            <div className="flex items-center justify-center gap-6 mt-10">
-              <Button
-                type="button"
-                className="theme-btn-default"
-                label="Cancel"
-                onClick={() => setVisible(false)}
-                disabled={removeUserLoading}
-                style={{
-                  fontSize: mobile ? "14px" : "22px",
-                }}
-              />
+              <div>
+                <div className="flex items-center justify-center gap-6 mt-10">
+                  <Button
+                    type="button"
+                    className="theme-btn-default"
+                    label="Cancel"
+                    onClick={() => setVisible(false)}
+                    disabled={removeUserLoading}
+                  />
 
-              {removeUserLoading ? (
-                <div
-                  className="theme-btn leading-none"
-                  style={{
-                    height: mobile ? "45px" : "55px",
-                    fontSize: mobile ? "14px" : "22px",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <DotLoader color="#fff" size={12} />
+                  {removeUserLoading ? (
+                    <div
+                      className="theme-btn leading-none"
+                      style={{
+                        height: "55px",
+                        fontSize: "22px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <DotLoader color="#fff" size={12} />
+                    </div>
+                  ) : (
+                    <Button
+                      type="button"
+                      className="theme-btn"
+                      label="Yes, delete this user"
+                      onClick={() => removeUser(selectedUser)}
+                      style={{
+                        fontSize: "22px",
+                      }}
+                    />
+                  )}
                 </div>
-              ) : (
-                <Button
-                  type="button"
-                  className="theme-btn"
-                  label={mobile ? "Yes" : "Yes, delete this user"}
-                  onClick={() => removeUser(selectedUser)}
-                  style={{
-                    fontSize: mobile ? "14px" : "22px",
-                  }}
-                />
-              )}
+              </div>
             </div>
-          </div>
-        </div>
+          </>
+        ) : (
+          <>
+            <div className="p-6 rounded-lg bg-white">
+              <div
+                className="flex justify-end cursor-pointer"
+                onClick={() => setVisible(false)}
+              >
+                <i
+                  style={{
+                    fontSize: "32px",
+                    color: "#8E9BAA",
+                  }}
+                  className="bx bx-x"
+                ></i>
+              </div>
+              <div className="flex justify-center items-center flex-col">
+                <h2 className="my-10 text-4xl text-gray-200 font-semibold">
+                  Are you sure?
+                </h2>
+                <div className="flex flex-col justify-center items-center">
+                  <div>You are about to delete: </div>
+                  <div className="text-blue font-semibold">
+                    {selectedUser?.userName}
+                  </div>
+                </div>
+                <div className="flex justify-center items-center">
+                  <div>You are about to delete: </div>
+                  <div className="text-blue font-semibold">{title}</div>
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center flex-col justify-center gap-6 mt-10">
+                  {removeUserLoading ? (
+                    <div className="theme-btn h-[48px] flex items-center justify-center w-full">
+                      <DotLoader color="#fff" size={12} />
+                    </div>
+                  ) : (
+                    <Button
+                      type="button"
+                      className="theme-btn w-full h-[48px]"
+                      label="Yes, delete this user"
+                      onClick={() => removeUser(selectedUser)}
+                      style={{
+                        fontSize: "20px",
+                      }}
+                    />
+                  )}
+
+                  <Button
+                    type="button"
+                    className="theme-btn-default leading-none w-full text-gray-100 h-[48px] font-normal text-[22px] rounded-lg mb-[50px]"
+                    label="Cancel"
+                    onClick={() => setVisible(false)}
+                    disabled={removeUserLoading}
+                  />
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </Dialog>
     </>
   );
